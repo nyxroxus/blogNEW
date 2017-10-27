bc83e87574342a1aaf87af42967190812c715ed3
NoxiousAlt <nyxroxus@gmail.com>
2017-10-27 07:48:09 +0200

    Styled header and footer nav

--- wp-content/themes/custom-theme/footer.php
+++ wp-content/themes/custom-theme/footer.php
@@ -1,11 +1,13 @@
-<footer class="clear">
-  <p>Blog template built with <a href="http://tachyons.io/">Tachyons</a> by <a href="https://twitter.com/NoxiousAlt">@NoxiousAlt</a>.</p>
-  <h1 class="f4-l f5 tr-l footer-navigation"><?php wp_nav_menu( array( 'theme_location'  => 'secondary', 'container_class' => 'secondary_menu' ) ); ?></h1>
-  <p>
-    <a href="#">Back to top</a>
-  </p>
+<footer class="clear c-bt--dashed c-bb--dashed bw2 b--redpink w-100 ph5 pv2">
+  <h1 class="f2-l f5 tc footer-navigation"><?php wp_nav_menu( array( 'theme_location'  => 'secondary', 'container_class' => 'secondary_menu' ) ); ?></h1>
+  <p class="darkergray tc">Blog template built with <a class="link redpink" href="http://tachyons.io/">Tachyons</a> by <a class="link redpink" href="https://twitter.com/NoxiousAlt">@NoxiousAlt</a>.</p>
+
 </footer>
+<p class="w-10 bg-redpink center tc">
+  <a class="fa fa-angle-up link white f2" href="#"></a>
+</p>
 <script src="https://use.fontawesome.com/65486b663f.js"></script>
+<!-- Enqueue this in functions.php -->
   <?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/custom-theme/header.php
+++ wp-content/themes/custom-theme/header.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <title><?php echo get_bloginfo( 'name' ) ?></title>
@@ -12,22 +11,12 @@
       <?php  wp_head(); ?>
   </head>
 <body>
-<div class="w-90 center mt3">
-    <div class="w-30-l w-70 fl fl-l" style="padding-top: 2%;">
-        <div class="" style="">
-            <?php the_custom_logo() ?>
-            <!-- Implement header logo -->
-        </div>
+<div class="w-70 center mt3">
+    <div class="w-100 center">
+      <?php the_custom_logo(); ?>
     </div>
-
-    <div class="w-70-l fr-l dn dib-l" style="">
+    <div class="bt bb b--redpink center tc" style="">
       <!-- Used before: <a class="" href="#"> wp_list_pages( '&title_li=' ); </a> -->
-        <h1 class="f4-l f5 tr-l header-navigation navigation"><?php wp_nav_menu( array( 'theme_location'  => 'primary', 'container_class' => 'primary_menu' ) ); ?></h1>
+        <h1 class="f5-l f5 header-navigation"><?php wp_nav_menu( array( 'theme_location'  => 'primary', 'container_class' => 'primary_menu' ) ); ?></h1>
     </div>
-    <div class="w-30 fr mt2 dib dn-l">
-        <i class="pointer fa fa-bars fa-3x fr custom-padding-menu"></i>
-        <!-- Add navigation menu functionality -->
-    </div>
-    <!-- <?php if(is_page( 'about-us' )) : ?><h3><?php the_title(); ?></h3><?php endif ?> -->
-
 </div>
