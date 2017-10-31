a47ec7955ff2a7f7d749267376446e15af8fb8c5
NoxiousAlt <nyxroxus@gmail.com>
2017-10-31 21:43:45 +0100

    ARK-1492: Theme settings

--- wp-content/themes/custom-theme/theme_settings_page.php
+++ wp-content/themes/custom-theme/theme_settings_page.php
@@ -1,3 +1,27 @@
-<?php
-echo "Hello World";
 ?>
+<div class="wrap">
+  <h1>Theme panel</h1>
+  <form method="post" action="options.php">
+
+      <?php
+        settings_fields("section");
+        do_settings_sections("theme-options");
+        submit_button();
+      ?>
+  </form>
+</div>
+<?php
+function display_facebook_url()
+{
+  ?>
+  <input type="text" name="fb_url" id="facebook_url" value="<?php echo get_option("facebook_url")?>"/>
+  <?php
+}
+function display_theme_panel_fields() {
+  add_settings_section("section", "All settings", null, "theme-options");
+  add_settings_field("facebook_url", "Facebook url" , "display_facebook_url" , "theme-options", "section");
+  register_setting("section", "facebook_url");
+}
+
+
+ ?>
