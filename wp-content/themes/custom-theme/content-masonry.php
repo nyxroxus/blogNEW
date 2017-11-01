26073482ed085a02ac18de6ec0180e2e41d8e162
NoxiousAlt <nyxroxus@gmail.com>
2017-11-01 14:32:18 +0100

    Created masonry layout

--- wp-content/themes/custom-theme/content-masonry.php
+++ wp-content/themes/custom-theme/content-masonry.php
@@ -1,13 +1,13 @@
-<article class="masonry-entry" id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+<article class="clear w-30 fl mr3 bg-darkpurple hover-bg-redpink mb3 mt2" id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
 <?php if ( has_post_thumbnail() ) : ?>
-    <div class="masonry-thumbnail">
+    <div class="">
         <a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('masonry-thumb'); ?></a>
-    </div><!--.masonry-thumbnail-->
+    </div>
 <?php endif; ?>
-    <div class="masonry-details">
-        <h5><a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><span class="masonry-post-title"> <?php the_title(); ?></span></a></h5>
-        <div class="masonry-post-excerpt">
+    <div class="">
+        <h5 class=""><a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><span class=""> <?php the_title(); ?></span></a></h5>
+        <div class="pa3">
             <?php the_excerpt(); ?>
-        </div><!--.masonry-post-excerpt-->
-    </div><!--/.masonry-entry-details -->
-</article><!--/.masonry-entry-->
+        </div>
+    </div>
+</article>
--- wp-content/themes/custom-theme/index.php
+++ wp-content/themes/custom-theme/index.php
@@ -1,22 +1,21 @@
 <?php get_header(); ?>
 
 <div class="w-100" style="">
-    <div class="w-60 pl5 fl-l">
-      <?php if ( have_posts() ) : ?>
-<div id="masonry-loop">
-  <?php /* The loop */ ?>
-  <?php while ( have_posts() ) : the_post(); ?>
-      <?php get_template_part( 'content', 'masonry' ); ?>
-  <?php endwhile; ?>
-</div>
-
-<?php else : ?>
-  <?php get_template_part( 'content', 'none' ); ?>
-<?php endif; ?>
+  <div class="w-60 pl5 fl-l">
+    <?php if ( have_posts() ) : ?>
+    <div id="masonry-loop">
+      <?php /* The loop */ ?>
+      <?php while ( have_posts() ) : the_post(); ?>
+          <?php get_template_part( 'content', 'masonry' ); ?>
+      <?php endwhile; ?>
     </div>
-    <div class="w-30 fr-l pr5">
-      <?php get_sidebar(); ?>
-    </div>
+    <?php else : ?>
+      <?php get_template_part( 'content', 'none' ); ?>
+    <?php endif; ?>
+  </div>
+  <div class="w-30 fr-l pr5">
+    <?php get_sidebar(); ?>
+  </div>
 </div>
 <div class="w-100" style="clear: both;">
   <?php get_footer(); ?>
