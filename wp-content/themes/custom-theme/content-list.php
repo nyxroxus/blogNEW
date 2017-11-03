82e4c85d850d209030f684a047f05663d2a355f7
NoxiousAlt <nyxroxus@gmail.com>
2017-11-03 15:33:36 +0100

    ARK-1500: Create and implement a sidebar

--- wp-content/themes/custom-theme/content-list.php
+++ wp-content/themes/custom-theme/content-list.php
@@ -1,14 +1,22 @@
-<div class="b--lightred bb bl pa3 mt3">
+<div class="pa3 mt3 bg-white w-70 mb3 shadow-4 fl">
 	<h2 class="pointer">
     <a class="lightred link" href="<?php the_permalink() ?>">
       <?php the_title(); ?>
     </a>
   </h2>
-	<p class="">
-    <?php the_date(); ?> by
+
+		<div class="">
+			<?php the_date(); ?>
+		</div>
     <a href="#" class="lightred link">
-      <?php the_author(); ?>
+			<div class="">
+    	<?php the_author(); ?>
+    	</div>
     </a>
-  </p>
-    <?php the_excerpt(); ?>
+		<div class="">
+			<?php the_excerpt(); ?>
+		</div>
+</div>
+<div class="w-30 fr">
+	<?php get_sidebar(); ?>
 </div>
