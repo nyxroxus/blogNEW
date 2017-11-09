415cd5b41cb9825856619fd7eff93f27ae65704a
NoxiousAlt <nyxroxus@gmail.com>
2017-11-09 22:52:15 +0100

    ARK-1536: Improve comments.php

--- wp-content/themes/custom-theme/comments.php
+++ wp-content/themes/custom-theme/comments.php
@@ -1,63 +1,31 @@
-<?php
-if ( post_password_required() ) {
-	return;
-}
-?>
+<?php if ( post_password_required() ) { return; } ?>
+<div id="" class="pt3 center pa3">
 
-<div id="comments" class="comments-area">
+<?php if ( have_comments() ) : ?>
+  <div class="">
+    <h2 class="">Comments:</h2>
+  	<?php the_comments_navigation(); ?>
+  	<ol class="bg-yellow">
+  		<?php
+  			wp_list_comments( array(
+  				'style'       => 'ol',
+  				'short_ping'  => true,
+  				'avatar_size' => 32,
+  			) );
+  		?>
+  	</ol>
+  </div>
+  <div class="">
+    <?php the_comments_navigation(); ?>
+  </div>
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				$comments_number = get_comments_number();
-				if ( 1 === $comments_number ) {
-					/* translators: %s: post title */
-					printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentysixteen' ), get_the_title() );
-				} else {
-					printf(
-						/* translators: 1: number of comments, 2: post title */
-						_nx(
-							'%1$s thought on &ldquo;%2$s&rdquo;',
-							'%1$s thoughts on &ldquo;%2$s&rdquo;',
-							$comments_number,
-							'comments title',
-							'twentysixteen'
-						),
-						number_format_i18n( $comments_number ),
-						get_the_title()
-					);
-				}
-			?>
-		</h2>
+<?php endif; ?>
+<?php if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+	<p class=""><?php _e( 'Comments are closed.', 'twentysixteen' ); ?></p>
+<?php endif; ?>
+<div class="w-70 center bg-green tc">
+  <?php comment_form( array( 'title_reply_before' => '<h2 id="" class="">', 'title_reply_after'  => '</h2>', ) ); ?>
+</div>
 
-		<?php the_comments_navigation(); ?>
 
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'       => 'ol',
-					'short_ping'  => true,
-					'avatar_size' => 42,
-				) );
-			?>
-		</ol><!-- .comment-list -->
-
-		<?php the_comments_navigation(); ?>
-
-	<?php endif; // Check for have_comments(). ?>
-
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentysixteen' ); ?></p>
-	<?php endif; ?>
-
-	<?php
-		comment_form( array(
-			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-			'title_reply_after'  => '</h2>',
-		) );
-	?>
-
-</div><!-- .comments-area -->
+</div>
--- wp-content/themes/custom-theme/single.php
+++ wp-content/themes/custom-theme/single.php
@@ -1,35 +1,59 @@
 <?php get_header(); ?>
-<article class="clear w-80 center pt3">
-<?php if (have_posts()) : while (have_posts()) : the_post();
-if ( comments_open() || get_comments_number() ) {
-  comments_template();
-}
-?>
+<article class="clear w-100 center pt3">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
   <div class="tc f2">
     <?php the_title(); ?>
   </div>
-  <div class="">
+  <div class="pa2 default" style="overflow: auto;">
+    <div class="w-90 center">
+      <div class="dib fl pa2 br4 default-clean-color">
+        <div class="default-clean-link-color">
+          <?php the_author_posts_link(); ?>
+        </div>
+      </div>
+      <div class="dib fr pa2 br4 default-clean-color">
+        <div class="default-clean-color">
+          <?php echo get_the_date(); ?>
+        </div>
+      </div>
+      <div class="dib pa2 br4 default-clean-color ml3">
+        <div class="default-clean-color">
+          <?php comments_number( 'There are no comments on this post', '1 comment', '2+ comments' ); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-80 center">
     <?php the_content(); ?>
   </div>
+  <div class="w-100 center">
+    <div class="tc">
+      <input type='button' style="border: none!important;" class="pa3 w-100 default-button-color" id='hideshow' value='View comments'>
+    </div>
 
 
-
+    <div id='comments' class="center w-90 bg-red mt3">
+      <?php
+      if ( comments_open() || get_comments_number() ) {
+        comments_template();
+      }
+      ?>
+    </div>
+  </div>
  <?php endwhile; ?>
  <?php endif; ?>
 </article>
-<div class="pa2" style="overflow: auto;">
-  <div class="w-80 center">
-    <div class="dib fl pa2 br4">
-      <?php the_author_posts_link(); ?>
-    </div>
 
-    <div class="dib fr pa2 br4">
-      <?php echo get_the_date(); ?>
-    </div>
-  </div>
-</div>
-<!-- Import comments and display them when the user click on a button 'show comments' -->
 <div class="" style="clear: both; margin-top: 50px;">
   <?php get_footer(); ?>
 </div>
+
+<!-- Script that has to go somewhere else  -->
+<script>
+jQuery(document).ready(function(){
+    jQuery('#hideshow').live('click', function(event) {
+         jQuery('#comments').toggle('show');
+    });
+});
+</script>
